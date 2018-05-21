2a999181b017adbc755e74e770d48ad317980f87
Kasra Nejad <pfdclover@gmail.com>
2018-05-21 09:34:57 -0400

    added scroll detection

--- src/script.js
+++ src/script.js
@@ -1,5 +1,13 @@
-"use strict"
-// window.addEventListener('click', () => {
-//   let portfolio = document.querySelector('#portfolio');
-//   portfolio.classList.add('active');
-// })
+(function() {
+  "use strict"
+  const portfolio = document.querySelector("#portfolio");
+
+  if (!portfolio.classList.contains("active")) {
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > window.innerHeight) {
+        portfolio.classList.add("active");
+        console.log(window.pageYOffset);
+      }
+    })
+  }
+})();
