09a1529f0f632a6a1a7545a1fb45bbb635b2a317
Kasra Nejad <pfdclover@gmail.com>
2018-08-25 10:54:52 -0400

    fixed vertical responsiveness

--- src/script.js
+++ src/script.js
@@ -1,16 +1,41 @@
 (function() {
-  "use strict"
+  "use strict";
   const portfolio = document.querySelector("#portfolio");
   const projects = document.querySelectorAll(".portfolio__project");
 
+  //highlight name
+  window.addEventListener("load", () => {
+    const letters = document.querySelectorAll(".letters");
+    let index = 0;
+    let offIndex = 0;
+
+    let onInterval = setInterval(() => {
+      if (index == 10) {
+        clearInterval(onInterval);
+      }
+
+      letters[index++].classList.add("active-text");
+    }, 50);
+
+    setTimeout(() => {
+      let offInterval = setInterval(() => {
+        if (offIndex == 10) {
+          clearInterval(offInterval);
+        }
+        letters[offIndex++].classList.remove("active-text");
+      }, 50);
+    }, 50);
+  });
+
+  // slide in projects
   if (!portfolio.classList.contains("active")) {
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       if (window.pageYOffset > window.innerHeight) {
         portfolio.classList.add("active");
         projects.forEach(project => {
-          project.classList.add("active")
-        })
+          project.classList.add("active");
+        });
       }
-    })
+    });
   }
 })();
