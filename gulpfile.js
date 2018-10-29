729226227a6cde24a6c937808c48c9462eff8336
kasra-nejad <pfdclover@gmail.com>
2018-10-29 12:42:06 +0000

    gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
 
 // Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-  return gulp.src(['src/sass/*.scss'])
+gulp.task("sass", function() {
+  return gulp
+    .src(["src/sass/*.scss"])
     .pipe(sass())
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
 
-
 // Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
+gulp.task("serve", ["sass"], function() {
   browserSync.init({
     server: "./src"
   });
 
-  gulp.watch(['src/sass/*.scss'], ['sass']);
-  gulp.watch("src/*.html").on('change', browserSync.reload);
-  gulp.watch("src/*.js").on('change', browserSync.reload);
+  gulp.watch(["src/sass/*.scss"], ["sass"]);
+  gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch("src/*.js").on("change", browserSync.reload);
+});
+
+//Gulp deploy
+gulp.task("deploy", function() {
+  return gulp.src("./dist/**/*").pipe(deploy());
 });
 
 // Default Task
-gulp.task('default', ['serve']);
+gulp.task("default", ["serve"]);
